f0a6f86f4e759c50da68e8ffebb6801352a798cf
DaisyQ <qiy13@mails.tsinghua.edu.cn>
2017-12-11 18:17:19 +0800

    Update app.js
    2 dices

--- app.js
+++ app.js
@@ -2,15 +2,14 @@
 GAME RULES:
 
 - The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- If a player rolls two 6 in a roll, he will lose his GLOBAL score. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
-- The first player to reach the winning score set in the input field on GLOBAL score wins the game
+- In each turn, a player rolls 2 dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, one of two dices is a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
 
 */
 
-var scores, roundScore, lastScore, activePlayer, gamePlaying, winningScore;
+var scores, roundScore, activePlayer, gamePlaying, winningScore;
 
 init();
 
@@ -18,28 +17,24 @@ init();
 document.querySelector('.btn-roll').addEventListener('click', function(){
     if (gamePlaying) {
         // 1. Produce a random number
-        var dice = Math.ceil(Math.random() * 6);
+        var dice1 = Math.ceil(Math.random() * 6);
+        var dice2 = Math.ceil(Math.random() * 6);
 
         // 2. Display the result
-        var diceDOM = document.querySelector('.dice');
-        diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' + dice + '.png';
+        var diceDOM1 = document.querySelector('.dice1');
+        diceDOM1.style.display = 'block';
+        diceDOM1.src = 'dice-' + dice1 + '.png';
         
-        console.log(dice);
+        var diceDOM2 = document.querySelector('.dice2');
+        diceDOM2.style.display = 'block';
+        diceDOM2.src = 'dice-' + dice2 + '.png';
+        
+        console.log(dice1 + " " + dice2);
         // 3. Update the round score IF the rolled number was NOT a 1
-        if (dice > 1) {
-            // two 6 in a row
-            if (lastScore === 6 && dice === 6) {
-                scores[activePlayer] = 0;
-                document.getElementById('score-' + activePlayer).textContent = 0;
-                nextPlayer();               
-            }
+        if (dice1 > 1 && dice2 > 1) {
             // Add score
-            else {
-                roundScore += dice;
-                lastScore = dice;
-                document.getElementById('current-' + activePlayer).textContent = roundScore;
-            }
+            roundScore += dice1 + dice2;
+            document.getElementById('current-' + activePlayer).textContent = roundScore;
         } else {
             // Next player
             nextPlayer()
@@ -59,7 +54,8 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         // check IF player won the game
         if (scores[activePlayer] >= winningScore) {
             document.getElementById('name-' + activePlayer).textContent = 'Winner!'; 
-            document.querySelector('.dice').style.display = 'none';  
+            document.querySelector('.dice1').style.display = 'none'; 
+            document.querySelector('.dice2').style.display = 'none'; 
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             gamePlaying = false;
@@ -96,7 +92,8 @@ function init(){
     document.querySelector('.player-1-panel').classList.remove('active');
     document.querySelector('.player-0-panel').classList.add('active');
     
-    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice1').style.display = 'none';
+    document.querySelector('.dice2').style.display = 'none';
 }
 
 // change to next player
@@ -109,5 +106,6 @@ function nextPlayer() {
     activePlayer = 1 - activePlayer;
     document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
         
-    document.querySelector('.dice').style.display = 'none';    
+    document.querySelector('.dice1').style.display = 'none'; 
+    document.querySelector('.dice2').style.display = 'none'; 
 }
